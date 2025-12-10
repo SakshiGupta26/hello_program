31d68e9fff36e9bb6a0be81af17252c815b5d9ad
yourUsername <yourEmail@provider.com>
2025-12-10 11:25:10 +0530

    Updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,6 @@
- //Rust Server with Rocket framework
+fn main() {
+      let emp_info:(&str,u8) = ("Ramesh",50);
+  let emp_name =emp_info.0;
+  let emp_age = emp_info.1;
+ println!("Employee Name={}, Employee Age{}",emp_name,emp_age);}
 
